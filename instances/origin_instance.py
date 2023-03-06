25eaa79001c55808f26434684f455c2c4af37c59
lplpl123 <758826560@qq.com>
2023-03-06 14:02:17 +0800

    202303061402

--- instances/origin_instance.py
+++ instances/origin_instance.py
@@ -2,4 +2,7 @@ from origins.origin import Origin
 
 
 class OriginInstance(Origin):
-    pass
+    def __init__(self):
+        super(OriginInstance, self).__init__()
+        self.yang_nums = self.yang_nums[2]
+        self.yin_nums = self.yin_nums[2]
